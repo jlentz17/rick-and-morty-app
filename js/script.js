638067e48d48d8b99cd17d441ad9077dfaa4934f
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-06-09 14:29:38 -0700

    added bumlma buttons and trying to add event listener

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var button = document.getElementById("button");
+var modal = document.getElementById("page-modal");
+// don't know how to do this
+button.addEventListener("click", function()){
+    modal.classList.add("is-active")
+}
