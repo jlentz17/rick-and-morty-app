5919935cbd3a18e191810ece7fc764f38a03aa2a
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-06-09 20:44:03 -0700

    added background-img and some javascript that doesn't work

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
-var button = document.getElementById("button");
-var modal = document.getElementById("page-modal");
+var button = document.querySelector("button");
+var modal = document.querySelector("page-modal");
 // don't know how to do this
-button.addEventListener("click", function()){
-    modal.classList.add("is-active")
+$("button").on("click", function(){
+    modal.style.display = "block"
+});
+
+
+
+
+function getSchwifty(data) {
+    var endPoint = `https://rickandmortyapi.com/api/character`
+    var currentrickAndMortyCharacter;
+    fetch(endPoint)
+    console.log(data)
+        .then((info)=> data.json())
+
 }
+
+getSchwifty();
