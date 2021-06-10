4f15c7630d07db0e1eb8ec607328ea9781e5c5dd
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-06-10 15:30:26 -0700

    working out fetch

--- js/script.js
+++ js/script.js
@@ -1,11 +1,44 @@
 var characterButton = document.getElementById("characterButton");
 
-var characterButton = document.getElementById("characterButton");
 
-var characterButton = document.getElementById("characterButton");
 var modal = document.querySelector(".modal");
 
 var deleteBtn = document.querySelector(".deleteBtn");
+
+var endPoint = `https://rickandmortyapi.com/api/character`
+
+var characterName = document.querySelector(".characterName");
+var characterEpisodes = document.querySelector(".characterEpsisodes");
+var characterLocation = document.querySelector(".characterLocation");
+
+var modalCardBody = document.querySelector(".modal-card-body");
+
+
+// I can't get the data to display on the screen.. the class is modal-card-body
+
+for (var i = 0; i < 20; i++){
+    fetch(endPoint)
+    // console.log(character);
+    .then(function(response) {
+        // console.log("resolved", response)
+        return response.json();
+    }).then(function(data) {
+        var character = data.results[i]
+    return character;
+})
+.catch(function(error) {
+    console.log("rejected", error)
+})
+
+
+function addRickAndMortyChar(character) {
+    modalCardBody.textContent = data.results[i];
+    console.log(character)
+}
+// console.log(endPoint)
+
+
+
 $("button").on("click", function(){
     console.log(modal)
     modal.classList.add("is-active")
@@ -13,10 +46,11 @@ $("button").on("click", function(){
 
 
 
-// don't know how to do this
+// don't know how to do this- nevermind!!!
 $(".deleteBtn").on("click", function(){
     modal.classList.remove("is-active")
 });
+};
 
 
 
@@ -25,7 +59,7 @@ $(".deleteBtn").on("click", function(){
 
 
 // function getSchwifty(data) {
-//     var endPoint = `https://rickandmortyapi.com/api/character`
+
 //     var currentrickAndMortyCharacter;
 //     fetch(endPoint)
 //     console.log(data)
@@ -33,4 +67,4 @@ $(".deleteBtn").on("click", function(){
 
 // }
 
-// getSchwifty();
+// getSchwifty():
