632ca654d55ecf77b7925f2956f183079e28d41f
Brody Beebe <bbeebe13@gmail.com>
2021-06-11 01:22:01 -0600

    added random character selector and name generates on click

--- js/script.js
+++ js/script.js
@@ -8,36 +8,43 @@ var deleteBtn = document.querySelector(".deleteBtn");
 var endPoint = `https://rickandmortyapi.com/api/character`
 
 var characterName = document.querySelector(".characterName");
+console.log(characterName);
 var characterEpisodes = document.querySelector(".characterEpsisodes");
 var characterLocation = document.querySelector(".characterLocation");
 
 var modalCardBody = document.querySelector(".modal-card-body");
 
-
 // I can't get the data to display on the screen.. the class is modal-card-body
 
     fetch(endPoint)
     // console.log(character);
-    .then(function(response) {
+    .then((response) => {
         // console.log("resolved", response)
-        return response.json();
-    }).then(function(data) {
-        for (var i = 0; i < 20; i++) {
-        character = data.results[i];
-        console.log(character)
-        return character;
-    }})
-    .catch(function(error) {
+        return response.json()
+    }).then((data) => {
+        characterObj = data.results
+    })
+    .catch((error) => {
         console.log("rejected", error)
     })
 
-// console.log(endPoint)
+var getRandomInt = function(min,max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+    
+var randomCharacterSelector = function() {
+    var i = getRandomInt(0,characterObj.length);
+    var character = characterObj[i];
+    return character;
+}
+
 
 
 
 $("button").on("click", function(){
+    var character = randomCharacterSelector();
     modalCardBody.textContent = character.name;
-    console.log(character)
     modal.classList.add("is-active")
 });
 
